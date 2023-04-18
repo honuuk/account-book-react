ae40e03624a03daa9b730371b235347f79f59c9e
jewook3617 <jewook3617@gmail.com>
2023-04-18 22:47:42 +0900

    [ADD] mui tabs

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,11 +3,17 @@ import dayjs from "dayjs";
 import { DatePicker } from "@mui/x-date-pickers";
 
 import "./home.css";
+import { Box, Tab, Tabs } from "@mui/material";
 
 interface Props {}
 
 const Home: React.FC<Props> = () => {
   const [selectedDate, setSelectedDate] = useState<dayjs.Dayjs>(dayjs());
+  const [tabIndex, setTabIndex] = useState<number>(0);
+  const handleTabClick = (
+    e: React.SyntheticEvent<Element, Event>,
+    index: number
+  ) => setTabIndex(index);
   return (
     <>
       <DatePicker
@@ -18,6 +24,12 @@ const Home: React.FC<Props> = () => {
         format="YYYY-MM"
         slotProps={{ textField: { disabled: true } }}
       />
+      <Box sx={{ borderBottom: 1, borderColor: "divider", marginTop: "16px" }}>
+        <Tabs value={tabIndex} onChange={handleTabClick}>
+          <Tab label="지출" />
+          <Tab label="수입" />
+        </Tabs>
+      </Box>
     </>
   );
 };
