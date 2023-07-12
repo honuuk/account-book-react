3168df6c69dded469def95977dd2e7aa2e267754
jewook3617 <jewook3617@gmail.com>
2023-07-12 21:01:08 +0900

    refactoring Dialog

--- src/components/page/Dialog.tsx
+++ src/components/page/Dialog.tsx
@@ -1,32 +1,55 @@
 import { Dialog } from "@mui/material";
 import { useForm } from "react-hook-form";
 
+import { SpendingCategory } from "../../types";
+
+import "./Dialog.css";
+
+interface FormValues {
+  title: string;
+  category: SpendingCategory;
+  price: number;
+  description: string;
+}
+
+const DefaultValues: FormValues = {
+  title: "",
+  category: "card",
+  price: 0,
+  description: "",
+};
+
 interface Props {
   close: () => void;
   opened: boolean;
+  initialValues?: FormValues;
 }
 
-const Comp: React.FC<Props> = ({ opened, close }) => {
-  const { register, handleSubmit } = useForm({
-    defaultValues: {
-      title: "",
-      category: "card",
-      price: undefined,
-      description: "",
-    },
+const Comp: React.FC<Props> = ({ opened, close, initialValues }) => {
+  const { register, handleSubmit, reset } = useForm({
+    defaultValues: initialValues || DefaultValues,
   });
-  const onSubmit = (data: any) => console.log(data);
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+    return close();
+  };
+
+  const handleClose = () => {
+    reset(DefaultValues);
+    close();
+  };
+
   return (
     <Dialog
       sx={{
         padding: "20px 100px",
       }}
-      onClose={close}
       open={opened}
     >
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="inputBox">
-          <text>지출내역 : </text>
+          <span>지출내역 : </span>
           <input
             type="text"
             placeholder="지출한 내역을 입력해주세요."
@@ -34,7 +57,7 @@ const Comp: React.FC<Props> = ({ opened, close }) => {
           />
         </div>
         <div className="inputBox">
-          <text>지출 카테고리 : </text>
+          <span>지출 카테고리 : </span>
           <input
             type="radio"
             id="card"
@@ -58,24 +81,27 @@ const Comp: React.FC<Props> = ({ opened, close }) => {
           <label htmlFor="welfare">복지</label>
         </div>
         <div className="inputBox">
-          <text>금액 : </text>
+          <span>금액 : </span>
           <input
             type="number"
             placeholder="금액을 입력해주세요."
             {...register("price")}
           />
-          <text>원</text>
+          <span>원</span>
         </div>
         <div className="inputBox">
-          <text>메모 : </text>
+          <span>메모 : </span>
           <input
             type="text"
             placeholder="메모를 입력해주세요."
             {...register("description")}
           />
         </div>
-        <button className="inputBotton" type="submit">
+        <button className="saveButton" type="submit">
           추가
+        </button>
+        <button className="cancelButton" onClick={handleClose}>
+          닫기
         </button>
       </form>
     </Dialog>
